b3b542cf778ba75524e2d9a033ca9359b4588e64
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2021-02-20 18:05:38 +0100

    Project added, env and edge cases cover

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# Getting Started with Create React App
+# Turning G-Spreadsheets into a REST API
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Hey! Check out this, is quiet nice way to connect to your spreadsheets via API. I didn't know that it was possible. For the whole explanation, check this [https://www.freecodecamp.org/news/react-and-googlesheets/](tutorial from freeCodeCamp) I followed from [https://github.com/nishant-666](@nishant-666). It's pretty well explained and super fast to do.
+
+## tl;dr
+
+For this you have to register for free in [https://sheet.best/](sheet.best), it turns spreadsheets into REST APIs for you to connect it to anything. Simple and fast, start now and see the possibilities. And an access to a Drive to create the file, obviously.
+
+Also here's the [https://create-react-app.dev/docs/adding-custom-environment-variables/#adding-development-environment-variables-in-env](documentation) for handling .env with `create-react-app`
 
 ## Available Scripts
 
@@ -14,11 +20,6 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
 
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
 ### `yarn build`
 
 Builds the app for production to the `build` folder.\
@@ -29,42 +30,5 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `yarn eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+with love 🖤, in berlin 🍻, with freecodecamp 👨‍💻 and vscode.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Button, Form, Container, Header } from 'semantic-ui-react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+require('dotenv').config()
+
+export default class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: '',
+            age: '',
+            salary: '',
+            hobby: '',
+            restartButton: false
+        }
+
+        this.changeHandler = this.changeHandler.bind(this)
+        this.handleError = this.handleError.bind(this)
+    }
+
+    changeHandler = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleRefresh = (e) => {
+        e.preventDefault()
+
+        window.location.reload()
+    }
+
+    handleError = () => {
+        const header = document.getElementById('header')
+        const form = document.getElementById('form')
+
+        this.setState({
+            restartButton: true
+        })
+
+        header.innerHTML = 'Mhmm, something went wrong'
+        form.innerHTML = 'Looks like something did work as expected, but no worries. Refresh and try again!'
+
+        console.log('Something didn\'t work as expected... Upsi 😅')
+    }
+
+    submitHandler = e => {
+        e.preventDefault()
+
+        axios.post('process.env.REACT_APP_SPREADSHEET_URL', this.state)
+            .then(res => {
+                const header = document.getElementById('header')
+                const form = document.getElementById('form')
+
+                if (res.status === 200) {
+                    header.innerHTML = 'Great! Thanks for submitting!'
+                    form.classList.add('hidden')
+                } else {
+                    this.handleError()
+                }
+            })
+            .catch(e => {
+                this.handleError()
+            })
+    }
+
+    render() {
+        const { name, age, salary, hobby } = this.state
+
+        return (
+            <Container fluid className="container">
+                <Header as='h2' id="header">React Google Sheets!</Header>
+                <Form className="form" id="form" onSubmit={this.submitHandler}>
+                    <Form.Field>
+                        <label>Name</label>
+                        <input
+                            placeholder='Enter your name'
+                            type="text"
+                            name="name"
+                            value={name}
+                            onChange={this.changeHandler}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Age</label>
+                        <input
+                            placeholder='Enter your age'
+                            type="number"
+                            name="age"
+                            value={age}
+                            onChange={this.changeHandler}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Salary</label>
+                        <input
+                            placeholder='Enter your salary'
+                            type="number"
+                            name="salary"
+                            value={salary}
+                            onChange={this.changeHandler}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Hobby</label>
+                        <input
+                            placeholder='Enter your hobby'
+                            type="text"
+                            name="hobby"
+                            value={hobby}
+                            onChange={this.changeHandler}
+                        />
+                    </Form.Field>
+
+                    <Button color="blue" type='submit'>Submit</Button>
+                </Form>
+
+                {this.state.restartButton &&
+                    <Button color="red" onClick={this.handleRefresh} id='refreshButton'>
+                        Refresh the page
+                    </Button>
+                }
+            </Container>
+        )
+    }
 }
-
-export default App;
